0107cba7dc859d69db5220b9f12792fa02671ffa
ryad haliawas <ryad@example.com>
2026-05-10 00:23:13 +0000

    fix missing screens

---
+++ lib/screens/hr_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HrScreen extends StatelessWidget {
+  const HrScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("الموارد البشرية")),
+      body: const Center(
+        child: Text("HR Screen جاهز"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/report_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReportScreen extends StatelessWidget {
+  const ReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("التقارير")),
+      body: const Center(
+        child: Text("Report Screen جاهز"),
+      ),
+    );
+  }
+}
